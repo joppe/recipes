c48d3b2a8960fecd91adff33723d774e74b5f21a
Joppe Aarts <joppe@apestaartje.info>
2020-11-04 09:08:42 +0100

    Provide a connection url for mongo instead of host/port

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,12 +1,7 @@
-const host = process.env.MONGO_HOST;
-const port = process.env.MONGO_PORT;
 const user = <string>process.env.MONGO_USER;
 const password = <string>process.env.MONGO_PASSWORD;
-const dbName = 'recipes';
 
-export const url = `mongodb+srv://${user}:${password}@${host}${
-    port ? `:${port}` : ''
-}/${dbName}?retryWrites=true&w=majority`;
+export const url = <string>process.env.MONGO_URL;
 
 export const options = {
     authSource: 'admin',
