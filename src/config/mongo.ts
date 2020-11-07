c0bfe3c8b216fab0288662f2230dca0ade6cdf36
Joppe Aarts <joppe@apestaartje.info>
2020-11-07 13:21:41 +0100

    Removed unused mongo config

--- src/config/mongo.ts
+++
@@ -1,7 +0,0 @@
-const host = process.env.MONGO_HOST;
-const port = process.env.MONGO_PORT;
-const user = process.env.MONGO_USER;
-const password = process.env.MONGO_PASSWORD;
-
-export const url = `mongodb://${user}:${password}@${host}:${port}`;
-export const dbName = 'recipes';
