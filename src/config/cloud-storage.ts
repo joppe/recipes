14537e146d7217ac2c8c23512213ecf5e382edd1
Joppe Aarts <joppe@apestaartje.info>
2020-11-07 13:21:29 +0100

    Get the name of the storage bucket from an env variable

--- src/config/cloud-storage.ts
+++ src/config/cloud-storage.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://storage.googleapis.com';
-export const BUCKET_NAME = 'recipe-images-2020';
+export const BUCKET_NAME = <string>process.env.NEXT_PUBLIC_GOOGLE_BUCKET;
