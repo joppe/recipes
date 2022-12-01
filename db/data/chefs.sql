4415539dee5d223c4d9909d515e0d44baae45050
joppe <joppe@apestaartje.info>
2022-12-01 20:03:11 +0100

    wip

--- db/data/chefs.sql
+++
@@ -1,4 +0,0 @@
-insert into chefs (id, "name" , "skill") values (gen_random_uuid(), 'Joppe', 4);
-insert into chefs (id, "name" , "skill") values (gen_random_uuid(), 'Gytha', 3);
-insert into chefs (id, "name" , "skill") values (gen_random_uuid(), 'Eline', 1);
-insert into chefs (id, "name" , "skill") values (gen_random_uuid(), 'Mulan', 1);
