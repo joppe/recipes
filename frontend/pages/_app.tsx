742f6380f27ab9962d8050b6f51334d724c7ab40
joppe <joppe@apestaartje.info>
2023-01-02 13:46:39 +0100

    Added Apollo context provider to NextJS

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css';
 
+import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { useApollo } from '../hooks/useApollo';
 
-export default MyApp;
+export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 const Home: NextPage = () => {
   return (
     <div>
+      <h1>This is the homepage</h1>
       <div className="bg-blue-600 text-gray-50">Hello</div>
       <div className="bg-red-600 text-gray-50">World</div>
       <Button>?</Button>
