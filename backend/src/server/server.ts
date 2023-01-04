6bf33dfea4abc5cb001a26533486298c73176ed8
joppe <joppe@apestaartje.info>
2023-01-04 19:13:48 +0100

    Added express for handling routes and added apollo server as middleware

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,5 +1,11 @@
+import http from 'http';
+
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
 
 import { getUserInfoFromToken } from '../auth/getUserInfoFromToken';
 import { chefsLoader } from '../loaders/chefsLoader';
@@ -13,29 +19,52 @@ import { prisma } from '../prisma/client';
 import { schema } from '../schema/schema';
 import { Context } from './Context';
 
+const PORT = 4000;
+
 export async function start() {
-  const server = new ApolloServer({
+  const app = express();
+
+  const httpServer = http.createServer(app);
+
+  const apolloServer = new ApolloServer({
     schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req, res }): Promise<Context> => {
-      const userInfo = getUserInfoFromToken(req.headers.authorization);
+  await apolloServer.start();
 
-      return {
-        prisma,
-        mediaLoader: mediaLoader(prisma),
-        ingredientsLoader: ingredientsLoader(prisma),
-        unitsLoader: unitsLoader(prisma),
-        recipesLoader: recipesLoader(prisma),
-        chefsLoader: chefsLoader(prisma),
-        mealsLoader: mealsLoader(prisma),
-        productsLoader: productsLoader(prisma),
-        userInfo,
-      };
-    },
+  app.post('/login', (req, res) => {
+    res.status(200).json({ ok: true });
   });
 
-  console.log(`🚀  Server ready at: ${url}`);
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(apolloServer, {
+      context: async ({ req, res }): Promise<Context> => {
+        const userInfo = getUserInfoFromToken(
+          req.headers['auth-token'] as string,
+        );
+
+        return {
+          prisma,
+          mediaLoader: mediaLoader(prisma),
+          ingredientsLoader: ingredientsLoader(prisma),
+          unitsLoader: unitsLoader(prisma),
+          recipesLoader: recipesLoader(prisma),
+          chefsLoader: chefsLoader(prisma),
+          mealsLoader: mealsLoader(prisma),
+          productsLoader: productsLoader(prisma),
+          userInfo,
+        };
+      },
+    }),
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve),
+  );
+
+  console.log(`🚀  Server ready at: https://localhost:${PORT}/`);
 }
