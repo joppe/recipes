759e8161d29822fdac3cd7a1860a575626ca2a24
joppe <joppe@apestaartje.info>
2023-01-18 20:51:30 +0100

    set prettier config in own package

---
+++ packages/config/prettier-config/index.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  importOrder: [
+    '^react/?(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '^@recipes/(.*)$',
+    '^../(.*)$',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
