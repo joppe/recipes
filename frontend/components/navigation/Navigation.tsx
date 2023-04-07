49433b4414ed67c24a8b190d43b9b3898b1b73cb
joppe <joppe@apestaartje.info>
2023-04-07 18:56:53 +0200

    Navigation styling

--- frontend/components/navigation/Navigation.tsx
+++ frontend/components/navigation/Navigation.tsx
@@ -10,7 +10,7 @@ export function Navigation({ children }: NavigationProps) {
   const { user } = useAuth();
 
   return (
-    <div className="w-full p-2 fixed flex justify-between">
+    <div className="w-full p-2 fixed top-0 flex justify-between shadow-md">
       <div>{user?.name}</div>
       <div className="flex gap-2">{children}</div>
     </div>
